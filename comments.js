24a8cb694f312a465a4ba30380e155e321c9dd48
Andrew <92085084+kojomensahonums@users.noreply.github.com>
2023-07-31 05:28:17 +0000

    Copilot third-variation-four commit

--- comments.js
+++ comments.js
@@ -1,15 +1,15 @@
-// Create web browser
-// http://localhost:3000/comments
+// // Create web browser
+// // http://localhost:3000/comments
 
-var express = require('express');
-var router = express.Router();
+// var express = require('express');
+// var router = express.Router();
 
-// Import the model
-var Comment = require('../models/comment');
+// // Import the model
+// var Comment = require('../models/comment');
 
-// Create a comment
-router.post('/', function(req, res) {
-  // Create a new comment using the Comment model
-  var comment = new Comment({
-    content: req.body.content,});
-  });
+// // Create a comment
+// router.post('/', function(req, res) {
+//   // Create a new comment using the Comment model
+//   var comment = new Comment({
+//     content: req.body.content,});
+//   });
