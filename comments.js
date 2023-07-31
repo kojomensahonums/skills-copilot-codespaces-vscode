ba0103fdf3b55ce018524fdfc1899d505281aeaa
Andrew <92085084+kojomensahonums@users.noreply.github.com>
2023-07-31 05:15:53 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,14 @@
+// Create web browser
+// http://localhost:3000/comments
+
+var express = require('express');
+var router = express.Router();
+
+// Import the model
+var Comment = require('../models/comment');
+
+// Create a comment
+router.post('/', function(req, res) {
+  // Create a new comment using the Comment model
+  var comment = new Comment({
+    content: req.body.content,
